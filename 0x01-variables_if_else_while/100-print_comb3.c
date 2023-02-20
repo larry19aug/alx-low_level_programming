10d2aa529604dab0067f825d7dfc22522ac3220d
Larry <larry19aug@gmail.com>
2023-02-20 15:11:08 -0800

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, n;
+
+	while (n = 0)
+	{
+		i = 0;
+
+		while (i < 10)
+		{
+			if (n != i && n < i)
+			{
+				putchar(n);
+				putchar(i);
+				if (i == 9 && n == 7)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
