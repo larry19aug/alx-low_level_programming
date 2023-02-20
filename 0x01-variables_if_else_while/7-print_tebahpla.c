2db3b6ba73e9880e6ec2889f672509901e09979e
Larry <larry19aug@gmail.com>
2023-02-20 01:58:55 -0800

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp = 'a';
+
+	for (alp = 'z'; alp >= 'a'; alp--)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
