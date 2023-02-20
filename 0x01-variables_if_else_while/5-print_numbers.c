fb5578e832becb2205f86b5dbbc5519987bd36d6
Larry <larry19aug@gmail.com>
2023-02-20 01:31:04 -0800

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i;
+
+	for (i = '1'; i < '10'; i++)
+		printf(i);
+	return (o);
+}
