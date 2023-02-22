2cc31914bb9e1a65346a245e8934c0f74e60656e
Larry <larry19aug@gmail.com>
2023-02-22 13:22:55 -0800

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+ 
+ void print_alphabet(void);
+{
+	char alp;
+	for (alp = 'a'; alp <= 'z'; alp++);
+	_putchar(alp);
+	_puctchar('\n');
+}
