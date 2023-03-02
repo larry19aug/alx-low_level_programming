be6ea8ca9238cf8377c315aa9e75f290fabac657
Larry <larry19aug@gmail.com>
2023-03-01 23:16:58 -0800

    tasks

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest:destination
+ * @src: source
+ * Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return(strcat(dest, src));
+}
