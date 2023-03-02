c161ef8aa5974e2267b76481b54090921e0c663f
Larry <larry19aug@gmail.com>
2023-03-02 00:01:54 -0800

    tasks

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ * @n: bytes
+ * Return: the pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	while (j < n)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + i) == '\0')
+			break;
+		i++;
+		j++;
+	}
+	return (dest);
+}
